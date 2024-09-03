845e72bae56a4af9e8305dfdbe18fa9077dec5cf
Sinan Aktepe <sinanaktepe@Sinans-MacBook-Pro.local>
2024-09-03 14:49:51 +0300

    initial

---
+++ lib/src/lang/ur.dart
@@ -0,0 +1,331 @@
+// Copyright 2024, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import '../default_localizations.dart';
+
+class UrLocalizations extends FirebaseUILocalizationLabels {
+  const UrLocalizations();
+
+  @override
+  String get accessDisabledErrorText => "اس اکاؤنٹ تک رسائی کو عارضی طور پر غیر فعال کر دیا گیا ہے۔";
+
+  @override
+  String get arrayLabel => "صف";
+
+  @override
+  String get booleanLabel => "bool";
+
+  @override
+  String get cancelLabel => "منسوخ";
+
+  @override
+  String get chooseACountry => "ملک منتخب کریں۔";
+
+  @override
+  String get confirmPasswordDoesNotMatchErrorText => "پاس ورڈز مماثل نہیں ہیں۔";
+
+  @override
+  String get confirmPasswordInputLabel => "پاس ورڈ کی تصدیق کریں۔";
+
+  @override
+  String get confirmPasswordIsRequiredErrorText => "اپنے پاس ورڈ کی تصدیق کریں۔";
+
+  @override
+  String get continueText => "جاری رکھیں";
+
+  @override
+  String get countryCode => "کوڈ";
+
+  @override
+  String get credentialAlreadyInUseErrorText => "یہ فراہم کنندہ ایک مختلف صارف اکاؤنٹ سے وابستہ ہے۔";
+
+  @override
+  String get deleteAccount => "اکاؤنٹ حذف کریں";
+
+  @override
+  String get differentMethodsSignInTitleText => "سائن ان کرنے کے لیے درج ذیل طریقوں میں سے کوئی ایک استعمال کریں۔";
+
+  @override
+  String get disable => "غیر فعال";
+
+  @override
+  String get eastInitialLabel => "ڈی";
+
+  @override
+  String get emailInputLabel => "ای میل ایڈریس";
+
+  @override
+  String get emailIsRequiredErrorText => "ای میل ایڈریس درکار ہے۔";
+
+  @override
+  String get emailLinkSignInButtonLabel => "لنک کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get emailTakenErrorText => "اس ای میل ایڈریس کے ساتھ پہلے سے ہی ایک اکاؤنٹ وابستہ ہے۔";
+
+  @override
+  String get enable => "چالو";
+
+  @override
+  String get enableMoreSignInMethods => "دوسرے سائن ان طریقوں کو فعال کریں۔";
+
+  @override
+  String get enterSMSCodeText => "ایس ایم ایس کوڈ درج کریں۔";
+
+  @override
+  String get findProviderForEmailTitleText => "جاری رکھنے کے لیے اپنا ای میل ایڈریس درج کریں۔";
+
+  @override
+  String get forgotPasswordButtonLabel => "اپنا پاس ورڈ بھول گئے؟";
+
+  @override
+  String get forgotPasswordHintText => "اپنا ای میل ایڈریس درج کریں۔ ہم آپ کو اپنا پاس ورڈ دوبارہ ترتیب دینے کے لیے ایک لنک بھیجیں گے۔";
+
+  @override
+  String get forgotPasswordViewTitle => "میں اپنا پاس ورڈ بھول گیا ہوں۔";
+
+  @override
+  String get geopointLabel => "جغرافیائی نقطہ";
+
+  @override
+  String get goBackButtonLabel => "واپس آو";
+
+  @override
+  String get invalidCountryCode => "غلط کوڈ";
+
+  @override
+  String get isNotAValidEmailErrorText => "ایک درست ای میل ایڈریس درج کریں۔";
+
+  @override
+  String get latitudeLabel => "عرض بلد";
+
+  @override
+  String get linkEmailButtonText => "اگلا";
+
+  @override
+  String get longitudeLabel => "طول البلد";
+
+  @override
+  String get mapLabel => "نقشہ";
+
+  @override
+  String get mfaTitle => "2 قدمی توثیق";
+
+  @override
+  String get name => "نام";
+
+  @override
+  String get northInitialLabel => "کے";
+
+  @override
+  String get nullLabel => "خالی";
+
+  @override
+  String get numberLabel => "نمبر";
+
+  @override
+  String get off => "بند";
+
+  @override
+  String get on => "کھولیں۔";
+
+  @override
+  String get passwordInputLabel => "پاس ورڈ";
+
+  @override
+  String get passwordIsRequiredErrorText => "پاس ورڈ درکار ہے۔";
+
+  @override
+  String get passwordResetEmailSentText => "ہم نے آپ کا پاس ورڈ دوبارہ ترتیب دینے کے لیے آپ کے ای میل ایڈریس پر ایک لنک بھیجا ہے۔ براہ کرم اپنا ای میل ان باکس چیک کریں۔";
+
+  @override
+  String get phoneInputLabel => "فون نمبر";
+
+  @override
+  String get phoneNumberInvalidErrorText => "فون نمبر غلط ہے۔";
+
+  @override
+  String get phoneNumberIsRequiredErrorText => "فون نمبر درکار ہے۔";
+
+  @override
+  String get phoneVerificationViewTitleText => "اپنا فون نمبر درج کریں۔";
+
+  @override
+  String get profile => "پروفائل";
+
+  @override
+  String get provideEmail => "اپنا ای میل ایڈریس اور پاس ورڈ درج کریں۔";
+
+  @override
+  String get referenceLabel => "حوالہ";
+
+  @override
+  String get registerActionText => "سائن اپ کریں۔";
+
+  @override
+  String get registerHintText => "اکاؤنٹ نہیں ہے؟";
+
+  @override
+  String get registerText => "سائن اپ کریں۔";
+
+  @override
+  String get resetPasswordButtonLabel => "پاس ورڈ دوبارہ ترتیب دیں";
+
+  @override
+  String get sendLinkButtonLabel => "لنک بھیجیں";
+
+  @override
+  String get signInActionText => "لاگ ان کریں۔";
+
+  @override
+  String get signInHintText => "کیا آپ کا اکاؤنٹ ہے؟";
+
+  @override
+  String get signInMethods => "لاگ ان کے طریقے";
+
+  @override
+  String get signInText => "سائن ان کریں۔";
+
+  @override
+  String get signInWithAppleButtonText => "ایپل کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get signInWithEmailLinkSentText => "ہم نے آپ کے ای میل ایڈریس پر ایک لنک بھیج دیا ہے۔ اپنا ای میل ان باکس چیک کریں اور لنک پر کلک کرکے سائن ان کریں۔";
+
+  @override
+  String get signInWithEmailLinkViewTitleText => "لنک کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get signInWithFacebookButtonText => "Facebook کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get signInWithGoogleButtonText => "گوگل کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get signInWithPhoneButtonText => "فون کے ذریعے سائن ان کریں۔";
+
+  @override
+  String get signInWithTwitterButtonText => "ٹویٹر کے ساتھ سائن ان کریں۔";
+
+  @override
+  String get signOutButtonText => "لاگ آؤٹ کریں۔";
+
+  @override
+  String get smsAutoresolutionFailedError => "SMS کوڈ کو خود بخود ڈی کوڈ نہیں کیا جا سکا۔ براہ کرم اپنا کوڈ دستی طور پر درج کریں۔";
+
+  @override
+  String get southInitialLabel => "جی";
+
+  @override
+  String get stringLabel => "تار";
+
+  @override
+  String get timestampLabel => "ٹائم سٹیمپ";
+
+  @override
+  String get typeLabel => "قسم";
+
+  @override
+  String get unknownError => "ایک نامعلوم خرابی پیش آگئی";
+
+  @override
+  String get updateLabel => "اپ ڈیٹ";
+
+  @override
+  String get userNotFoundErrorText => "اکاؤنٹ نہیں ملا";
+
+  @override
+  String get valueLabel => "قدر";
+
+  @override
+  String get verifyCodeButtonText => "تصدیق کریں";
+
+  @override
+  String get verifyingSMSCodeText => "ایس ایم ایس کوڈ کی تصدیق ہو رہی ہے...";
+
+  @override
+  String get verifyItsYouText => "تصدیق کریں کہ آپ ہیں۔";
+
+  @override
+  String get verifyPhoneNumberButtonText => "اگلا";
+
+  @override
+  String get westInitialLabel => "بی۔";
+
+  @override
+  String get wrongOrNoPasswordErrorText => "آپ کا درج کردہ پاس ورڈ غلط ہے یا صارف کے پاس پاس ورڈ نہیں ہے۔";
+
+  @override
+  String get uploadButtonText => "Upload file";
+
+  @override
+  String get verifyEmailTitle => "Verify your email";
+
+  @override
+  String get verificationEmailSentText =>
+      "A verification email has been sent to your email address. Please check your email and click on the link to verify your email address.";
+
+  @override
+  String get verificationFailedText =>
+      "We couldn't verify your email address. ";
+
+  @override
+  String get resendVerificationEmailButtonLabel => "Resend verification email";
+
+  @override
+  String get verificationEmailSentTextShort => "Verification email sent";
+
+  @override
+  String get emailIsNotVerifiedText => "Email is not verified";
+
+  @override
+  String get waitingForEmailVerificationText =>
+      "Waiting for email verification";
+
+  @override
+  String get dismissButtonLabel => "Dismiss";
+
+  @override
+  String get okButtonLabel => "OK";
+
+  @override
+  String get checkEmailHintText =>
+      "Please check your email and click the link to verify your email address.";
+
+  @override
+  String get doneButtonLabel => "Done";
+
+  @override
+  String get invalidVerificationCodeErrorText =>
+      "The code you entered is invalid. Please try again.";
+
+  @override
+  String get ulinkProviderAlertTitle => "Unlink provider";
+
+  @override
+  String get confirmUnlinkButtonLabel => "Unlink";
+
+  @override
+  String get cancelButtonLabel => "Cancel";
+
+  @override
+  String get unlinkProviderAlertMessage =>
+      "Are you sure you want to unlink this provider?";
+
+  @override
+  String get weakPasswordErrorText =>
+      "Password should be at least 6 characters";
+
+  @override
+  String get confirmDeleteAccountAlertTitle => "Confirm account deletion";
+
+  @override
+  String get confirmDeleteAccountAlertMessage =>
+      "Are you sure you want to delete your account?";
+
+  @override
+  String get confirmDeleteAccountButtonLabel => "Yes, delete";
+
+  @override
+  String get sendVerificationEmailLabel => "Send verification email";
+}
